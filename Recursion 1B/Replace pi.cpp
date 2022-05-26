f40616060d3e7d50cd9e343be48e0e709b3da7ed
Anant Gupta <anantg2001@gmail.com>
2022-05-26 20:08:57 +0530

    Initial Commit

---
+++ Recursion 1B/Replace pi.cpp
@@ -0,0 +1,41 @@
+// Replace pi (recursive)
+// Send Feedback
+// Given a string, compute recursively a new string where all appearances of "pi" have been replaced by "3.14".
+// Constraints :
+// 1 <= |S| <= 50
+// where |S| represents the length of string S. 
+// Sample Input 1 :
+// xpix
+// Sample Output :
+// x3.14x
+// Sample Input 2 :
+// pipi
+// Sample Output :
+// 3.143.14
+// Sample Input 3 :
+// pip
+// Sample Output :
+// 3.14p
+// Constraints:-
+// 1<=|S|<=50
+
+
+
+void replacePi(char input[]) {
+	// Write your code here
+  
+	int length = strlen(input);
+
+	if (length==1)
+		return;
+     replacePi(input+1);
+	if((input[0]=='p') and (input[1]=='i')){
+		for(int i=strlen(input);i>=2;i--){
+			input[i+2]=input[i];
+		}
+		input[0]='3';
+		input[1]='.';
+		input[2]='1';
+		input[3]='4';
+	}
+}
